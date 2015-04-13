f04bacc7dbfc06afbbc35f862f214ea22ce3250d
jtrussell <jus.russell@gmail.com>
2015-04-13 18:48:45 -0400

    Initial commit

---
+++ www/js/app.js
@@ -0,0 +1,45 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('deck', ['ionic', 'deck.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/app.html',
+    controller: 'AppCtrl',
+    controllerAs: 'app'
+  })
+
+  .state('app.poker', {
+    url: '/poker',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/card.html',
+      }
+    }
+  })
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/poker');
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('deck.controllers', [])
+
+.controller('AppCtrl', function($scope) {
+  var decks = $scope.decks = {
+    goat: {
+      name: 'Mountain Goat Deck',
+      cards: ['?', 0, '1/2', 1, 2, 3, 5, 8, 13, 20, 40, 100, '&infin;']
+    }
+  };
+
+  $scope.card = "Swipe to select a card &rarr;"
+
+  $scope.chooseCard = function(card) {
+    $scope.card = card;
+  };
+
+  useDeck = function(deckId) {
+    $scope.deck = decks[deckId];
+  };
+
+  useDeck('goat');
+})
+
+.controller('CardsCtrl', function() {
+  // ...
+})
