eb8ac468529c9a3232cf58ed96fd99e00ee6c54a
Justin <jus.russell@gmail.com>
2015-04-16 11:31:21 -0400

    fix: Rename deck

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,7 @@ angular.module('deck.controllers', [])
 .controller('AppCtrl', function($scope) {
   var decks = $scope.decks = {
     goat: {
-      name: 'Mountain Goat Deck',
+      name: 'Goat Deck',
       cards: ['?', 0, '1/2', 1, 2, 3, 5, 8, 13, 20, 40, 100, '&infin;']
     }
   };
