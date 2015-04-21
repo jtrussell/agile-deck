c69d4f8018f136101f8257954316f844b41cc8d1
Justin <jus.russell@gmail.com>
2015-04-21 17:04:21 -0400

    feature: Add deck switching
    With fibonacci deck!
    
    Closes #1

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,26 +1,72 @@
 angular.module('deck.controllers', [])
 
-.controller('AppCtrl', function($scope) {
-  var decks = $scope.decks = {
+.controller('AppCtrl', function($scope, $ionicModal) {
+  var card, deck, decks = {
     goat: {
       name: 'Goat Deck',
       cards: ['?', 0, '1/2', 1, 2, 3, 5, 8, 13, 20, 40, 100, '&infin;']
+    },
+    fibonacci: {
+      name: 'Fibonacci',
+      cards: ['?', 0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, '&infin;']
     }
   };
 
-  $scope.card = "&rarr;"
+  var getCard = function() {
+    return card;
+  };
 
-  $scope.chooseCard = function(card) {
+  var setCard = function(card) {
     $scope.card = card;
   };
 
-  useDeck = function(deckId) {
-    $scope.deck = decks[deckId];
+  var changeDeckModal; // To be filled in
+
+  var changeDeck = function() {
+    $ionicModal.fromTemplateUrl('templates/change-deck-modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      changeDeckModal = modal;
+      modal.show();
+    });
   };
 
-  useDeck('goat');
+  var getDeck = function() {
+    return deck;
+  };
+
+  var setDeck = function(deckId) {
+    if(angular.isString(deckId)) {
+      deck = decks[deckId];
+    } else {
+      deck = deckId;
+    }
+    setCard(deck.cards[0]);
+    if(changeDeckModal) {
+      changeDeckModal.hide();
+    }
+  };
+
+  var getDecks = function() {
+    return decks;
+  };
+
+  setDeck('goat');
+  setCard('&rarr;');
+
+  angular.extend($scope, {
+    changeDeck: changeDeck,
+    getCard: getCard,
+    getDeck: getDeck,
+    getDecks: getDecks,
+    setCard: setCard,
+    setDeck: setDeck
+  });
 })
 
 .controller('CardsCtrl', function() {
   // ...
 })
+
+; // Done
